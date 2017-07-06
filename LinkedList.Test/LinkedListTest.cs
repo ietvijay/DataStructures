6da4f729fd7facfa953d2a48b26f0e785776293c
vijay yadav <iet.vijay@gmail.com>
2017-07-05 23:27:02 -0400

    VY- Added Linked list apis

---
+++ LinkedList.Test/LinkedListTest.cs
@@ -0,0 +1,98 @@
+﻿using Sample.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+namespace LinkedList.Test
+{
+    [TestClass]
+    public class LinkedListTest
+    {
+        #region ADDFIRST
+        [TestMethod]
+        public void AddFirst_Test()
+        {
+            LinkedList<int> linkList = new LinkedList<int>();
+            linkList.AddFirst(10);
+            Assert.AreEqual(10, linkList.Head.Value);
+        }
+        [TestMethod]
+        public void AddFirst_AddingNodeToExistingNode()
+        {
+            LinkedList<int> linkList = new LinkedList<int>();
+
+            //Two nodes added
+            linkList.Head = new Node<int>() { Value = 10 };
+            linkList.Head.Next = new Node<int> { Value = 20 };
+
+            linkList.AddFirst(5);
+            Assert.AreEqual(5, linkList.Head.Value);
+        }
+        #endregion
+
+        #region AddBeforeNode
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void AddBeforeNode_BlankList()
+        {
+            LinkedList<int> linkList = new LinkedList<int>();
+
+            //Two nodes added
+            linkList.AddBeforeNode(10, 5);
+
+        }
+
+        [TestMethod]
+        public void AddBeforeNode_OnlyHeadNodeList()
+        {
+            LinkedList<int> linkList = new LinkedList<int>();
+
+            //Two nodes added
+            linkList.Head = new Node<int>() { Value = 10 };
+
+            linkList.AddBeforeNode(10, 5);
+            Assert.AreEqual(5, linkList.Head.Value);
+        }
+
+        [TestMethod]
+        public void AddBeforeNode_FullList()
+        {
+            LinkedList<int> linkList = new LinkedList<int>();
+
+            //Two nodes added
+            linkList.Head = new Node<int>() { Value = 10 };
+            linkList.Head.Next = new Node<int> { Value = 20 };
+            linkList.Head.Next.Next = new Node<int> { Value = 30 };
+            linkList.AddBeforeNode(20, 15);
+            Assert.AreEqual(15, linkList.Head.Next.Value);
+        }
+
+        [TestMethod]
+        public void AddBeforeNode_Tail()
+        {
+            LinkedList<int> linkList = new LinkedList<int>();
+
+            //Two nodes added
+            linkList.Head = new Node<int>() { Value = 10 };
+            linkList.Head.Next = new Node<int> { Value = 20 };
+            linkList.Head.Next.Next = new Node<int> { Value = 30 };
+            linkList.AddBeforeNode(30, 15);
+            Assert.AreEqual(15, linkList.Head.Next.Next.Value);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void AddBeforeNode_NodeDoesNotExits()
+        {
+            LinkedList<int> linkList = new LinkedList<int>();
+
+            //Two nodes added
+            linkList.Head = new Node<int>() { Value = 10 };
+            linkList.Head.Next = new Node<int> { Value = 20 };
+            linkList.Head.Next.Next = new Node<int> { Value = 30 };
+            linkList.AddBeforeNode(50, 15);
+
+        }
+        #endregion
+
+    }
+    
+}
---
+++ LinkedList/Models/LinkedList.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Models
+{
+    public class LinkedList<T>
+    {
+
+        public Node<T> Head { get; set; }
+        public Node<T> Tail { get; set; }
+        public void AddFirst(T value)
+        {
+            Node<T> newNode = new Node<T>();
+            newNode.Value = value;
+            if (Head == null)
+            {
+                Head = Tail = newNode;
+            }
+
+            else
+            {
+                newNode.Next = Head;
+                Head = newNode;
+            }
+
+        }
+        public void AddFirst(Node<T> newNode)
+        {
+
+            if (Head == null)
+            {
+                Head = Tail = newNode;
+            }
+
+            else
+            {
+                newNode.Next = Head;
+                Head = newNode;
+            }
+
+        }
+
+        //public void AddBeforeNode( T existingNodevalue,T newNodeValue)
+        //{
+        //    if (Head == null)
+        //    {
+        //        throw new Exception("List is empty");
+        //    }
+        //    Node<T> currentNode = Head;
+        //    Node<T> currentPrevNode = null;
+        //    while (currentNode != node)
+        //    {
+        //        if (currentNode == Tail)
+        //        {
+        //            throw new Exception("node does not exists in the linked list");
+        //        }
+        //        currentPrevNode = currentNode;
+        //        currentNode = currentNode.Next;
+        //    }
+        //    currentPrevNode.Next = newNode;
+        //    newNode.Next = currentNode;
+
+        //}
+        public void AddAfterNode(T existingNodevalue, T newNodeValue)
+        {
+        }
+        public void RemoveFirst(T existingNodevalue, T newNodeValue)
+        {
+
+        }
+
+
+        public void RemoveAt(int n)
+        {
+            if (Head == null)
+            {
+                throw new NullReferenceException("List is blank");
+            }
+
+            Node<T> currentNode = Head;
+            Node<T> previousCurrentNode = null;
+
+            if (n == 1)
+            {
+                Head = Head.Next;
+                return;
+            }
+            int i = 0;
+            while (i < n - 1)
+            {
+                previousCurrentNode = currentNode;
+                currentNode = currentNode.Next;
+                i++;
+            }
+            if (currentNode.Next == null)
+            {
+                previousCurrentNode.Next = null;
+            }
+            else
+            {
+                previousCurrentNode.Next = currentNode.Next;
+            }
+
+        }
+        public void RemoveLast(T existingNodevalue, T newNodeValue)
+        {
+        }
+        public void RemoveNode(T value)
+        {
+        }
+        public void AddBeforeNode(T existingNodeValue, T newNodeValue)
+        {
+            Node<T> newNode = new Node<T>() { Value = newNodeValue };
+
+            Node<T> currentNode = Head;
+            Node<T> previousNode = null;
+
+            if (existingNodeValue.Equals(Head.Value))
+            {
+                newNode.Next = Head;
+                Head = newNode;
+            }
+            else
+            {
+                while (!currentNode.Value.Equals(existingNodeValue))
+                {
+                    if (currentNode.Next == null)
+                    {
+                        throw new NullReferenceException("Node not found");
+                    }
+                    previousNode = currentNode;
+                    currentNode = currentNode.Next;
+                }
+                previousNode.Next = newNode;
+                newNode.Next = currentNode;
+
+            }
+
+
+
+        }
+
+
+        public IEnumerable<T> GetNodes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
